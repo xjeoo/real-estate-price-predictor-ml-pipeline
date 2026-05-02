90fcbd805e3a35a8b5c49255b0c6590e34859d73
xjeoo <fnaffoxy1234@gmail.com>
2026-05-02 21:01:17 +0300

    Initial commit: sitemap links extraction logic

---
+++ README.md
@@ -0,0 +1,5 @@
+# Real Estate Price AI Predictor - ML Pipeline
+
+This project is a dedicated machine learning pipeline designed to predict property prices. The system automates the entire flow from raw data collection to model deployment.
+
+The pipeline consists of three main stages: automated data collection from real estate listings, comprehensive data cleaning and preprocessing, and the training/testing of machine learning models to estimate market values.
---
+++ get_links.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+cwd = os.getcwd()
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept-Language': 'ro-RO,ro;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Referer': 'https://www.google.ro/'
+}
+
+xml_apartments_for_sale= os.getenv("XML_SITEMAP")
+
+content = requests.get(xml_apartments_for_sale, headers=headers).text
+soup = BeautifulSoup(content, 'xml')
+
+urls = soup.find_all('url')
+
+## PAGES TO SCRAPE
+links = {
+    "links": []
+}
+for url in urls:
+    loc = url.find('loc')
+    link = loc.text
+    links["links"].append(link)
+
+links_df = pd.DataFrame(links)
+path = os.path.join(cwd, 'data', 'sitemap_links.csv')
+links_df.to_csv(path, index=False)
+
